e80268989da8c589d23d07b130aeb5a6281e0110
Qborll <mainvarues@gmail.com>
2022-08-20 13:51:19 +0300

    updating code with loops

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt("Сколько фильмом вы уже посмотрели",'');
+const numberOfFilms = +prompt("Сколько фильмом вы уже посмотрели",'');
 
 const personalMovieDB = {
     count:numberOfFilms,
@@ -9,13 +9,29 @@ const personalMovieDB = {
 
 };
 
-const lastUserFilms = prompt("Один из последних просмотрених фильмом",''),
-      scoreFilms = prompt("На сколько оцените его",''),
-      lastUserFilms2 = prompt("Один из последних просмотрених фильмом",''),
-      scoreFilms2 = prompt("На сколько оцените его",'');
 
+for( let i = 0; i<2;i++ ){
 
-personalMovieDB.movies[lastUserFilms]=scoreFilms;
-personalMovieDB.movies[lastUserFilms2]=scoreFilms2;
+    const lastUserFilms = prompt("Один из последних просмотрених фильмом",''),
+          scoreFilms = prompt("На сколько оцените его",'');
 
+          if(lastUserFilms.length < 50 && lastUserFilms!=null && scoreFilms!=null && lastUserFilms != "" && scoreFilms != "" && scoreFilms.length < 50){
+            personalMovieDB.movies[lastUserFilms]=scoreFilms;
+            console.log('done');
+          } else{
+            console.log('error');
+            i--;
+          }
+
+}
+
+if(personalMovieDB.count < 10){
+    alert("Вам присвоен статут 'Новичек'");
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    alert("Вам присвоен статут 'Шарящий'");
+} else if (personalMovieDB.count >= 30 ){
+    alert("Вам присвоен статут 'Are you crazy'");
+} else {
+    console.log('Ошибка');
+}
 console.log(personalMovieDB);
