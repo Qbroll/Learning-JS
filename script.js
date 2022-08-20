2dd2008a499647fc6946ba89681ca0a53e3b501c
Qborll <mainvarues@gmail.com>
2022-08-20 13:17:41 +0300

    work with loops

--- script.js
+++ script.js
@@ -32,3 +32,35 @@ while(i>=2 && i<=16){
     }
        
 }
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// Пишем решение вот тут
+for(let i = 0; i< data.length; i++){
+    if(typeof(data[i])==='string')
+    {
+        data[i]=`${data[i]} - done`;
+    }else{
+        data[i]=data[i]*2;
+    }
+}
+
+console.log(data);
+
+
+const data1 = [5, 10, 'Shopping', 20, 'Homework'];
+const result1 = [];
+
+const lines = 5;
+let exit = '';
+
+for (let i = lines; i<lines; i++){
+    for(let j = 0; j<lines-i; j++ ){
+        exit+=" ";
+    }
+    for(let k = 0; k<i; k++ ){
+        exit+="*";
+    }
+    exit+="\n";
+}
+console.log(exit);
